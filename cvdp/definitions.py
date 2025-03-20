3b6e078e8cf5e1cceac012327e48dd0906887416
justin-richling <richling@ucar.edu>
2025-03-20 09:42:29 -0600

    Update definitions.py

--- cvdp/definitions.py
+++ cvdp/definitions.py
@@ -2,7 +2,11 @@ import os
 
 
 PATH_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-PATH_COLORMAPS_DIR = f"{PATH_ROOT_DIR}/visualization/colormaps/"
-PATH_VARIABLE_DEFAULTS = f"{PATH_ROOT_DIR}/variable_defaults.yml"
+CURRENT_DIR = os.getcwd()
+PARENT_DIR = os.path.dirname(CURRENT_DIR)
+print(os.path.dirname(CURRENT_DIR))
+print("PATH_ROOT_DIR",PATH_ROOT_DIR)
+PATH_COLORMAPS_DIR = f"{PATH_ROOT_DIR}/visualization/colormaps/" # might move to config yaml for user definition?
+PATH_VARIABLE_DEFAULTS = f"{PARENT_DIR}/variable_defaults.yml" # might move to config yaml for user definition?
 PATH_LANDSEA_MASK_NC = f"{PATH_ROOT_DIR}/utils/landsea.nc"
 PATH_BANNER_PNG = f"{PATH_ROOT_DIR}/visualization/banner.png"
