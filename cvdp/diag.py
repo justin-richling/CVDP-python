65c8f6c341ba8daa678a2032c9707c225406978b
justin-richling <richling@ucar.edu>
2025-04-08 10:34:18 -0600

    Update diag.py

--- cvdp/diag.py
+++ cvdp/diag.py
@@ -5,4 +5,5 @@ diag.py
 Centralizes diagnostic (computation) functions to a single import script
 """
 
-from cvdp.computation.climatology import compute_seasonal_avgs, compute_seasonal_stds
+#from cvdp.computation.climatology import compute_seasonal_avgs, compute_seasonal_stds
+from computation.climatology import compute_seasonal_avgs, compute_seasonal_stds
