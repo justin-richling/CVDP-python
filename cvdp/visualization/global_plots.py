24c1f4fe70f1e058a8039ebef4e29a5efa41619c
justin-richling <richling@ucar.edu>
2025-04-08 13:52:48 -0600

    Update global_plots.py

--- cvdp/visualization/global_plots.py
+++ cvdp/visualization/global_plots.py
@@ -316,7 +316,7 @@ def stacked_global_latlon_plot(vn, finarrs, arrs, plot_dict, title, plot_name, p
 '''
 
 from vis import *
-from vis_utils import *
+from visualization.vis_utils import *
 import old_utils.avg_functions as af
 ncl_masks = af.ncl_masks
 
