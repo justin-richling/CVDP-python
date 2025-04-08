924802ba3ecd1b8435bafdd2443a83074ff82947
justin-richling <richling@ucar.edu>
2025-04-08 10:36:51 -0600

    Update vis.py

--- cvdp/vis.py
+++ cvdp/vis.py
@@ -5,6 +5,10 @@ vis.py
 Centralizes visualization functions to a single import script
 """
 
-from cvdp.visualization.colormaps import get_NCL_colormap
+"""from cvdp.visualization.colormaps import get_NCL_colormap
 from cvdp.visualization.seasonal_plots import *
-from cvdp.visualization.notebook_build import *
+from cvdp.visualization.notebook_build import *"""
+
+from visualization.colormaps import get_NCL_colormap
+from visualization.seasonal_plots import *
+from visualization.notebook_build import *
