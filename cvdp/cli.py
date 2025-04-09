cad3709ac83c2f2545154221dd17cab1e2a4bae9
justin-richling <richling@ucar.edu>
2025-04-08 18:06:29 -0600

    Update cli.py

--- cvdp/cli.py
+++ cvdp/cli.py
@@ -71,13 +71,17 @@ def main():
     sim_0 = list(sim_datasets.keys())[0]
     ref_seas_avgs = compute_seasonal_avgs(ref_datasets[ref_0][vn])
     if "member" in ref_seas_avgs.coords:
+        attrs = ref_seas_avgs.attrs  # save before doing groupby/mean
         members = ref_seas_avgs.member
         ref_seas_avgs = ref_seas_avgs.mean(dim="member")
+        ref_seas_avgs.attrs = attrs
         ref_seas_avgs.attrs["members"] = members
     sim_seas_avgs = compute_seasonal_avgs(sim_datasets[sim_0][vn])
     if "member" in sim_seas_avgs.coords:
+        attrs = sim_seas_avgs.attrs  # save before doing groupby/mean
         members = sim_seas_avgs.member
         sim_seas_avgs = sim_seas_avgs.mean(dim="member")
+        sim_seas_avgs.attrs = attrs
         sim_seas_avgs.attrs["members"] = members
     print("AHHHH",sim_seas_avgs,"\n\n")
 
