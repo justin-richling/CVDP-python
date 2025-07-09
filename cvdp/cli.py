8fc630ff21fccee3efacc40475b6b6c5f6e1f0ea
justin-richling <richling@ucar.edu>
2025-07-09 12:17:01 -0600

    Update cli.py

--- cvdp/cli.py
+++ cvdp/cli.py
@@ -69,15 +69,15 @@ def main():
 
     vns = ["psl"]
     for vn in vns:
-        ref_seas_avgs, sim_seas_avgs, arr_diff = mean_seasonal_calc(ref_datasets[ref_0][vn], sim_datasets[sim_0][vn])
-        arr_prime = an.interp_diff(sim_seas_avgs, ref_seas_avgs)
+        ref_seas_avgs, sim_seas_avgs, arr_diff = mean_seasonal_calc(ref_datasets[ref_0][vn], sim_datasets[sim_0][vn], vn)
+        """arr_prime = an.interp_diff(sim_seas_avgs, ref_seas_avgs)
 
         # If arr_prime is None, then the two runs have already been interpolated (TS -> SST) or are the same grid/shape
         if arr_prime is None:
             arr_diff = sim_seas_avgs - ref_seas_avgs
         else:
             arr_diff = (arr_prime - ref_seas_avgs)
-        #from visualization.AtmOcnGR import graphics
+        #from visualization.AtmOcnGR import graphics"""
         kwargs = {"ref_seas":ref_seas_avgs, "sim_seas":sim_seas_avgs,
                 "diff_seas":arr_diff}
         graphics(plot_loc, **kwargs)
