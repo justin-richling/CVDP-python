db550c94c8a6f56e770e62b3d0afbb8d7d38c3ea
justin-richling <richling@ucar.edu>
2025-07-09 16:35:52 -0600

    Update AtmOcnGR.py

--- cvdp/vis/AtmOcnGR.py
+++ cvdp/vis/AtmOcnGR.py
@@ -111,12 +111,12 @@ def graphics(plot_loc, **kwargs):
                     for plot_type in ["summary", "indmem","indmemdiff"]:
                         if plot_type == "summary":
                             if map_type == "global":
-                                if type == "spatialmean":
+                                """if type == "spatialmean":
                                     sim_seas_avg = sim_seas_avgs[f"{vn}_{type}_{season.lower()}"].mean(dim="time")
                                     ref_seas_avg = ref_seas_avgs[f"{vn}_{type}_{season.lower()}"].mean(dim="time")
                                     seas_avg_diff = seas_avgs_diff[f"{vn}_{type}_{season.lower()}"].mean(dim="time")
                                     plot_name, title = get_plot_name_and_title(vn, None, type, season, plot_type, map_type)
-                                    fig = global_ensemble_plot([sim_seas_avg, ref_seas_avg], seas_avg_diff, vn, type, vtres, title)
+                                    fig = global_ensemble_plot([sim_seas_avg, ref_seas_avg], seas_avg_diff, vn, type, vtres, title)"""
                                 if type == "trends":
                                     sim_seas_avg, sim_res, sim_fit = af.lin_regress(sim_seas_avgs[f"{vn}_{type}_{season.lower()}"])
                                     ref_seas_avg, ref_res, res_fit = af.lin_regress(ref_seas_avgs[f"{vn}_{type}_{season.lower()}"])
