364d723a196b3d7c60c133f44ca27eb121a23449
justin-richling <richling@ucar.edu>
2025-07-14 16:36:09 -0600

    Update AtmOcnGR.py

--- cvdp/vis/AtmOcnGR.py
+++ cvdp/vis/AtmOcnGR.py
@@ -111,7 +111,7 @@ def get_plot_name_and_title(vn, var, ptype, season, plot_type, map_type):
                 "indmemdiff": f"NPI Pattern Differences ({season_upper})\n",
             }[plot_type]
             results.append((plot_name, title))
-        else:
+        elif season != "NDJFM":
             suffix = f"{ptype}_{season_lower}.{plot_type}.png"
             plot_name = f"{base_var}_{suffix}" if ptype != "trends" else f"{base_var}_pattern_{season_lower}.{plot_type}.png"
             title = {
