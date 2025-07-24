992f15f4067a1a422cb890417bc3995d6d1013b8
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-07-24 13:55:10 -0600

    Update definitions.py

--- cvdp/definitions.py
+++ cvdp/definitions.py
@@ -3,7 +3,8 @@ import os
 
 PATH_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CURRENT_DIR = os.getcwd()
-PARENT_DIR = os.path.dirname(CURRENT_DIR)
+#PARENT_DIR = os.path.dirname(CURRENT_DIR)
+PARENT_DIR = os.path.dirname(PATH_ROOT_DIR)
 print(os.path.dirname(CURRENT_DIR))
 print("PATH_ROOT_DIR",PATH_ROOT_DIR)
 PATH_COLORMAPS_DIR = f"{PATH_ROOT_DIR}/vis/colormaps/" # might move to config yaml for user definition?
