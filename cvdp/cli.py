966ed98007d220cdf444733474c53ebe70c6ff44
Justin Richling <richling@ucar.edu>
2026-02-07 18:06:31 -0700

    Update kwargs for further downstream scripts

--- cvdp/cli.py
+++ cvdp/cli.py
@@ -55,7 +55,68 @@ def main():
     #from io import get_input_data
     from file_io import get_input_data
     #from cvdp.io import get_input_data
-    print("PARENT_DIR",PARENT_DIR)
+    print("PARENT_DIR",PARENT_DIR,"\n")
+
+
+
+
+    """
+    #def mean_seasonal_calc(ds_name, dataset, var_name, config_dict):
+    save_loc = config_dict[ds_name]["save_loc"]
+    Path(save_loc).mkdir(parents=True, exist_ok=True)
+    syr = config_dict[ds_name]["syr"]
+    eyr = config_dict[ds_name]["eyr"]
+
+    ts_filename = f'{ds_name}.cvdp_data.{var_name}.climo.ts.{syr}-{eyr}.nc'
+    ts_fno = save_loc / Path(ts_filename)
+
+    data_dict = {}
+    calc_all_mean = False
+    #if ts_fno.is_file():
+    if "members" in config_dict[ds_name]:
+        print("AtmOcnMean.py:  members are in this case:",ds_name)
+
+        if ts_fno.is_file():
+            seas_ts = xr.open_dataset(ts_fno)
+        members = config_dict[ds_name]["members"]
+        for member in members:
+            ts_mem_filename = f'{ds_name}.cvdp_data.{var_name}{member}climo.ts.{syr}-{eyr}.nc'
+            ts_mem_fno = save_loc / Path(ts_mem_filename)
+
+            ts_mem_mean_filename = f'{ds_name}.cvdp_data.{var_name}{member}climo.ts.mean.{syr}-{eyr}.nc'
+            ts_mem_mean_fno = save_loc / Path(ts_mem_mean_filename)
+
+            if ts_mem_fno.is_file() and ts_mem_mean_fno.is_file():
+                print(f"\tFound pre-existing climatology files for {ds_name} {var_name} {member}, loading from disk...\n")
+                seas_mem_ts = xr.open_dataset(ts_mem_fno)
+                data_dict[f"seas_ts{member[:-1]}"] = seas_mem_ts
+
+                seas_mem_mean_ts = xr.open_dataset(ts_mem_mean_fno)
+                data_dict[f"seas_ts{member[:-1]}_mean"] = seas_mem_mean_ts
+                calc_all_mean = False
+
+        # Average all members if applicable    
+        if calc_all_mean:
+            #seas_ts = xr.open_dataset(ts_fno)
+            seas_ts = seas_ts.mean(dim="member", keep_attrs=True)
+            seas_ts.attrs["members"] = members
+            seas_ts.to_netcdf(ts_fno)
+            print(f"\tSUCCESS: Climatological seasonal mean over members saved to file: {ts_fno}\n")
+        #else:
+        #    seas_ts = xr.open_dataset(ts_fno)
+    else:
+        print("AtmOcnMean.py:  members are NOT in this case:",ds_name)
+        if ts_fno.is_file():
+            print(f"\tFound pre-existing climatology files for {ds_name} {var_name}, loading from disk...\n")
+            seas_ts = xr.open_dataset(ts_fno)
+
+    """
+
+
+
+
+
+
 
     # These are dictionaries of datasets
     ref_datasets, sim_datasets, config_dict = get_input_data(f"{PARENT_DIR}/example_config.yaml")
@@ -71,26 +132,43 @@ def main():
         #print(f"\nProcessing variable: {vn}\n")
         for ref_name in ref_names:
             print(f"Trying reference {ref_name} for climatologies")
-            data_dict = mean_seasonal_calc(ref_name, ref_datasets[ref_name][vn],
+            if ref_name not in kwargs:
+                kwargs[ref_name] = {}
+            #kwargs[ref_name]["run_type"] = "reference"
+            kwargs[f"{ref_name}_run_type"] = "reference"
+            ref_var = ref_datasets[ref_name][vn]
+            data_dict = mean_seasonal_calc(ref_name, ref_var,
                                                vn, config_dict)
-            kwargs[f"{ref_name}_season_trnd_avgs"] = ref_datasets[ref_name][vn]
+            kwargs[f"{ref_name}_season_trnd_avgs"] = ref_var
             ref_seas_ts = data_dict["seas_ts"]
             kwargs[ref_name] = ref_seas_ts
             if "members" in ref_seas_ts.attrs:
                 members = ref_seas_ts.attrs["members"]
+                kwargs[ref_name]["members"] = members
                 for member in members:
                     kwargs[f"{ref_name}{member[:-1]}"] = data_dict[f"seas_ts{member[:-1]}"]
         for sim_name in sim_names:
             print(f"Trying simulation {sim_name} for climatologies")
-            data_dict = mean_seasonal_calc(sim_name, sim_datasets[sim_name][vn],
+            if sim_name not in kwargs:
+                kwargs[sim_name] = {}
+            kwargs[f"{sim_name}_run_type"] = "simulation"
+            sim_var = sim_datasets[sim_name][vn]
+            data_dict = mean_seasonal_calc(sim_name, sim_var,
                                                vn, config_dict)
-            kwargs[f"{sim_name}_season_trnd_avgs"] = sim_datasets[sim_name][vn]
+            kwargs[f"{sim_name}_season_trnd_avgs"] = sim_var
             sim_seas_ts = data_dict["seas_ts"]
             kwargs[sim_name] = sim_seas_ts
+
             if "members" in sim_seas_ts.attrs:
+                members_sub = config_dict[sim_name]["members"]
                 members = sim_seas_ts.attrs["members"]
-                for member in members:
-                    kwargs[f"{sim_name}{member[:-1]}"] = data_dict[f"seas_ts{member[:-1]}"]
+                huh = [mem for mem in members if mem in members_sub]
+                kwargs[f"{sim_name}_members"] = huh
+                for member in huh:
+                    try:
+                        kwargs[f"{sim_name}{member[:-1]}"] = data_dict[f"seas_ts{member[:-1]}"]
+                    except KeyError as e:
+                        print(f"seas_ts{member[:-1]}")
 
         kwargs["ref_seas_ts"] = ref_seas_ts
         kwargs["sim_seas_ts"] = sim_seas_ts
